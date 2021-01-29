6702f624f54d964ebf26bbffff50bfba3c7f202b
t4ccer <64430288+t4ccer@users.noreply.github.com>
2021-01-29 22:32:35 +0100

    Add --rm option

--- src/Lp.hs
+++ src/Lp.hs
@@ -1,21 +1,26 @@
 module Lp  where
 
+import           Data.List          (isPrefixOf)
 import           System.Environment
+import System.Exit (exitFailure)
 
 run :: IO ()
 run = do
   fpaths <- getArgs
-  mapM_ runOne fpaths
+  let removeAfter = "--rm" `elem` fpaths
+      fs = filter (not . isPrefixOf "-") fpaths
+  mapM_ (runOne undefined ) fpaths
 
-runOne :: FilePath -> IO ()
-runOne fpath = do
+runOne :: Bool -> FilePath -> IO ()
+runOne removeAfter fpath = do
   firstLine <- words .  head . lines <$> readFile fpath
   if head firstLine == "#!lp"
     then do
       let path = last firstLine
       readFile fpath >>= writeFile (path <> "/" <> last (wordsBy '/' fpath))
-    else putStrLn $ "File '" <> fpath <> "' does not contain lp pragma"
-
+    else do
+      putStrLn $ "File '" <> fpath <> "' does not contain lp pragma"
+      exitFailure
 
 wordsBy :: Char -> [Char] -> [[Char]]
 wordsBy c s =  case dropWhile {-partain:Char.-}(==c) s of
