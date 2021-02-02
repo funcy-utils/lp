33b5167704dca746c20d0e5d7fa36fea415f1917
t4ccer <t4ccer@gmail.com>
2021-02-02 15:01:27 +0000

    Implement --rm

--- src/Lp.hs
+++ src/Lp.hs
@@ -1,15 +1,17 @@
 module Lp  where
 
+import           Control.Monad      (when)
 import           Data.List          (isPrefixOf)
-import           System.Environment
-import System.Exit (exitFailure)
+import           System.Directory   (removeFile)
+import           System.Environment (getArgs)
+import           System.Exit        (exitFailure)
 
 run :: IO ()
 run = do
   fpaths <- getArgs
   let removeAfter = "--rm" `elem` fpaths
       fs = filter (not . isPrefixOf "-") fpaths
-  mapM_ (runOne undefined ) fpaths
+  mapM_ (runOne removeAfter) fs
 
 runOne :: Bool -> FilePath -> IO ()
 runOne removeAfter fpath = do
@@ -17,7 +19,8 @@ runOne removeAfter fpath = do
   if head firstLine == "#!lp"
     then do
       let path = last firstLine
-      readFile fpath >>= writeFile (path <> "/" <> last (wordsBy '/' fpath))
+      readFile fpath >>= writeFile (path <> "/" <> last (wordsBy '/' fpath)) . unlines . tail . lines
+      when removeAfter (removeFile fpath)
     else do
       putStrLn $ "File '" <> fpath <> "' does not contain lp pragma"
       exitFailure
@@ -27,3 +30,4 @@ wordsBy c s =  case dropWhile {-partain:Char.-}(==c) s of
   "" -> []
   s' -> w : wordsBy c s''
     where (w, s'') = break {-partain:Char.-} (==c) s'
+
